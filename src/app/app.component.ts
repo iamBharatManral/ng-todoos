6992c8d3a6fbfc5e043b4ba4086383a876dc8935
iamBharatManral <justbharatmanral@gmail.com>
2023-06-26 07:33:45 +0530

    feat: add data model for todo app and basic app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TodoList} from "./core/TodoList";
+import {TodoItem} from "./core/TodoItem";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-todoos';
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers", false),
+    new TodoItem("Collect tickets", false)
+  ])
+  get username(): string{
+    return this.list.user
+  }
+  get itemCount(): number{
+    return this.list.items.filter(item => !item.complete).length
+  }
 }
